a979783c988a3a5405fe5632103aec9f58ae8390
SwatYou <qtodjzb.qyahzn@gmail.com>
2025-11-07 02:37:32 -0600

    t

--- Project/Microservices/apiGateway/src/main/java/com/example/gateway/ApiGatewayApplication.java
+++ Project/Microservices/apiGateway/src/main/java/com/example/gateway/ApiGatewayApplication.java
@@ -1,7 +1,8 @@
-package com.example.demo;
+package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
