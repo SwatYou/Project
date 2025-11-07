f92b289a5fbca4d9490df94fc623a411ee055df2
SwatYou <qtodjzb.qyahzn@gmail.com>
2025-11-07 01:06:07 -0600

    Changes

--- Project/Microservices/users/src/main/java/api/aurora/net/UsersController.java
+++ Project/Microservices/users/src/main/java/api/aurora/net/UsersController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MyRestController{
+public class UsersController{
 
-    @GetMapping("/main")
+    @GetMapping("")
     public String main(){
         return "hello";
     }
